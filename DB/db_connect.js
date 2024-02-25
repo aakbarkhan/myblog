f2a329096145de13a0e54a82c6e33dcb9a867398
aakbarkhan <aakuukhan@gmail.com>
2024-02-25 21:35:40 +0530

    add the navbar and newBlog

--- DB/db_connect.js
+++ DB/db_connect.js
@@ -14,6 +14,7 @@ connection.connect(function(err) {
     return;
   }
   console.log('Connected as id ' + connection.threadId);
+
 });
 
 function handleQueryError(error) {
@@ -23,6 +24,56 @@ function handleQueryError(error) {
     throw error;
   }
 }
+
+// connection.query('DROP DATABASE IF EXISTS ownDoc', function (error, results, fields) {
+//   handleQueryError(error);
+//   console.log('Database dropped if it existed');
+
+//   connection.query('CREATE DATABASE ownDoc', function (error, results, fields) {
+//     handleQueryError(error);
+//     console.log('Database created');
+
+//     connection.query('USE ownDoc', function (error, results, fields) {
+//       handleQueryError(error);
+//       console.log('Switched to database ownDoc');
+
+//       const sql = `CREATE TABLE blogs(
+//         id INT AUTO_increment PRIMARY KEY,
+//         title VARCHAR(255) NOT NULL,
+//         content TEXT,
+//         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+//       )`;
+
+//       connection.query(sql, function (error, results, fields) {
+//         handleQueryError(error);
+//         console.log('Table created');
+//       });
+        
+//       const insertSql = `INSERT INTO blogs (title, content) VALUES ?`;
+//       const values = [
+//           ['First Blog', 'This is the first blog.'],
+//           ['Second Blog', 'This is the second blog.'],
+//           ['third Blog', 'This is the third blog.'],
+//           // Add more blogs as needed
+//       ];
+  
+//       connection.query(insertSql, [values], function (error, results, fields) {
+//         handleQueryError(error);
+//         console.log('Data inserted');
+//         // connection.end();
+//       });
+
+//       connection.query('SELECT title, content FROM blogs', function (error, results, fields) {
+//         if (error) throw error;
+//         console.log(results);
+//         // connection.end();
+//       })
+//     });
+//   });
+//   // connection.end();
+
+  
+// });
 
 connection.query('DROP DATABASE IF EXISTS ownDoc', function (error, results, fields) {
   handleQueryError(error);
@@ -38,38 +89,39 @@ connection.query('DROP DATABASE IF EXISTS ownDoc', function (error, results, fie
 
       const sql = `CREATE TABLE blogs(
         id INT AUTO_increment PRIMARY KEY,
-        title VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL ,
         content TEXT,
+        image VARCHAR(255),
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
       )`;
 
       connection.query(sql, function (error, results, fields) {
         handleQueryError(error);
-        console.log('Table created');
-      });
-        
-      const insertSql = `INSERT INTO blogs (title, content) VALUES ?`;
-      const values = [
-          ['First Blog', 'This is the first blog.'],
-          ['Second Blog', 'This is the second blog.'],
-          ['third Blog', 'This is the third blog.'],
-          // Add more blogs as needed
-      ];
-  
-      connection.query(insertSql, [values], function (error, results, fields) {
-        handleQueryError(error);
-        console.log('Data inserted');
-        // connection.end();
-      });
+        console.log('Table created');        
+        const insertSql = `INSERT INTO blogs (title, content) VALUES ?`;
+        const values = [
+            ['First Blog', 'This is the first blog.'],
+            ['Second Blog', 'This is the second blog.'],
+            ['third Blog', 'This is the third blog.'],
+        ];
+    
+        connection.query(insertSql, [values], function (error, results, fields) {
+          handleQueryError(error);
+          console.log('Data inserted');
+          // connection.end();
+        });
 
-      connection.query('SELECT title, content FROM blogs', function (error, results, fields) {
-        if (error) throw error;
-        console.log(results);
-        // connection.end();
-      })
+        connection.query('SELECT title, content FROM blogs', function (error, results, fields) {
+          if (error) throw error;
+          console.log(results);
+          // connection.end();
+        });
+      });
     });
   });
-  // connection.end();
 });
 
+
 module.exports = connection;
+
+
--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 import BlogList from './components/BlogList';
+import { Routes, Route, BrowserRouter as Router   } from 'react-router-dom';
+// import NewBlog from './components/newPage';
+import Navbar from './components/navBar'; 
+import NewBlog from './components/newPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <BlogList />
-      </header>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/blogs' Component={BlogList} />
+        <Route path='/new' Component={NewBlog} />
+      </Routes>
+    </Router>
     </div>
   );
 }
